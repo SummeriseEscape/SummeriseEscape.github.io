d47a7c49f7bddf100da014447464fad705900a3b
SummeriseEscape <3467328673@qq.com>
2026-05-11 23:41:47 +0800

    fix music player

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["src/content/posts/moonlit-sea.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmoonlit-sea.mdx&astroContentModuleFlag=true")],
+["src/content/posts/night-train.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fnight-train.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/starry-summer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fstarry-summer.mdx&astroContentModuleFlag=true")],
-["src/content/posts/night-train.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fnight-train.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/moonlit-sea.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmoonlit-sea.mdx&astroContentModuleFlag=true")]]);
 		
